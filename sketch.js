825ae3385814ae37d0d6ad647265f85811e9408e
clemehhhh <clementhajjar@gmail.com>
2025-05-02 20:51:32 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background(500);
+  
+  // Set the text size
+  textSize(32);
+  
+  // Set the text color
+  fill(50);
+  
+  // Display the text at coordinates (50, 200)
+  text('', 50, 200);
+}
